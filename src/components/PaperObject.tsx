6567e5c40fe3e6d3e10b0cd8618571de39d11403
itymarcel <marcel@hypersuper.uk>
2025-04-05 11:43:09 +0200

    up: add images

--- src/components/PaperObject.tsx
+++ src/components/PaperObject.tsx
@@ -6,7 +6,10 @@ interface PaperObjectProps {
 const PaperObject: React.FC<PaperObjectProps> = ({data, key}) => {
     return (
         <div className="paper-object-container p-4">
-            <div className="paper-object">
+            <div className="paper-object flex flex-col gap-2">
+                <div className="image-container w-full rounded-md aspect-square">
+                    <img src={data.image_url} alt='' className="w-full h-full" />
+                </div>
                 <h1 className="text-4xl">
                     {data.title_org}
                 </h1>
